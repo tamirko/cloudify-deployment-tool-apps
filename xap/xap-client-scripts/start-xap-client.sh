e8c232ab4aed3a135b03ef3712366ab91131f109
tamirko <tamirkorem@gmail.com>
2016-06-06 11:44:39 +0300

    added xap client v2

--- xap/xap-client-scripts/start-xap-client.sh
+++ xap/xap-client-scripts/start-xap-client.sh
@@ -3,10 +3,18 @@ set -x
 YUM_CMD=$(which yum)
 APT_GET_CMD=$(which apt-get)
 
+deployment_id=$(ctx deployment id)
 
 cd ~/
 touch xxx
 touch yyy
+pushd ~/.ssh/
+private_key_name="private_key_${deployment_id}"
+ssh-keygen -t rsa -C "xap@gigaspaces.com" -f "${private_key_name}" -q -N ""
+chmod 400 ${private_key_name}
+cp -rp ${private_key_name} ~/
+cat ${private_key_name}.pub >> authorized_keys
+popd
 COMMAND="python -m SimpleHTTPServer 8000"
 ctx logger info "${COMMAND}"
 nohup ${COMMAND} > /dev/null 2>&1 &
