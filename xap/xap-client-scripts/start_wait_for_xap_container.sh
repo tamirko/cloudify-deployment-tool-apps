66cc5dab90607f81476c1add5c5a907e61fae8b8
tamirko <tamirkorem@gmail.com>
2016-06-25 20:21:35 +0300

    added get private key 2

--- xap/xap-client-scripts/start_wait_for_xap_container.sh
+++ xap/xap-client-scripts/start_wait_for_xap_container.sh
@@ -6,14 +6,18 @@ APT_GET_CMD=$(which apt-get)
 deployment_id=$(ctx deployment id)
 
 
-pushd ~/.ssh/
+ctx logger info "id is `id`"
+
+pushd /home/ubuntu/.ssh/
 
 private_key_name_prefix="private_key_${deployment_id}"
 private_key_name="${private_key_name_prefix}.pem"
 #windows_private_key_name="${private_key_name_prefix}.ppk"
 
 client_ip=$(ctx target instance runtime-properties client_ip_addr)
-wget ${client_ip}:8000/${private_key_name}.pub
+wget http://${client_ip}:8000/${private_key_name}.pub
+currStatus=$?
+ctx logger info "wget http://${client_ip}:8000/${private_key_name}.pub status is ${currStatus}"
 chmod 644 ${private_key_name}.pub
 cat ${private_key_name}.pub >> authorized_keys
 
